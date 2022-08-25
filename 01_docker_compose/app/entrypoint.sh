6ec5c21ad21bf86604b6207a85d649f4002861bc
katelocate <katelocat3@gmail.com>
2022-08-25 21:04:15 +0300

    Add instructions on how to apply migrations and fill the database in container.

---
+++ 01_docker_compose/app/entrypoint.sh
@@ -0,0 +1,22 @@
+#!/bin/sh
+
+# first apply all migrations
+
+while ! python manage.py migrate; do
+  sleep 5
+done
+
+# then create superuser from django settings
+
+python manage.py createsuperuser --noinput
+
+# after that migrate the sqlite data to the postgres database
+
+cd ./data_loader
+python ./load_data.py
+
+cd ../
+
+sh run_uwsgi.sh
+
+exec "$@"
