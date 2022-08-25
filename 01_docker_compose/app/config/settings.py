6ef1c936c3743e3e6aaab5c5f7ab7c33e7dd3d6a
katelocate <katelocat3@gmail.com>
2022-08-25 15:58:00 +0300

    Replace the ALLOWED_HOSTS parameter with a variable from the server.env file.

--- 01_docker_compose/app/config/settings.py
+++ 01_docker_compose/app/config/settings.py
@@ -16,7 +16,7 @@ DJANGO_SUPERUSER_EMAIL = os.environ.get('DJANGO_SUPERUSER_EMAIL')
 
 DEBUG = os.environ.get('DEBUG', False)
 
-ALLOWED_HOSTS = ['127.0.0.1', '0.0.0.0', 'localhost']
+ALLOWED_HOSTS = os.environ.get('DJANGO_ALLOWED_HOSTS').split(' ')
 
 if DEBUG:
     import socket
