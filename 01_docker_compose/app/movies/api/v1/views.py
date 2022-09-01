5f77ca8a721738bfb36a3b5344b0d5a152c2d651
katelocate <katelocat3@gmail.com>
2022-09-01 20:51:08 +0300

    agg_movies_query

--- 01_docker_compose/app/movies/api/v1/views.py
+++ 01_docker_compose/app/movies/api/v1/views.py
@@ -8,58 +8,56 @@ from ...models import Filmwork
 
 
 class MoviesListApi(BaseListView):
-    #  Fields needed:
-    #   id, title, description, creation_date, rating, type,
-    #   genre_name (list),
-    #   actors (list of names), directors (list of names), writers (list of names),
-
     """Represents a view class for serializing information about movies."""
 
     model = Filmwork
     http_method_names = ['get']
 
-    def get_queryset(self):
-        """Method of getting database objects and preparing it"""
-        movies_query = self.model.objects.prefetch_related('genres', 'personfilmwork')
-        movies = movies_query.values('id', 'title', 'description', 'creation_date', 'rating', 'type')
-        movies_with_aggregated_fields = movies.annotate(
-            genres=ArrayAgg('genres__name', distinct=True),
-        ).annotate(
-            actors=ArrayAgg('personfilmwork__person__full_name', distinct=True, filter=Q(personfilmwork__role='actor')),
-            writers=ArrayAgg('personfilmwork__person__full_name', distinct=True, filter=Q(personfilmwork__role='writer')),
-            directors=ArrayAgg('personfilmwork__person__full_name', distinct=True, filter=Q(personfilmwork__role='director')),
+    @staticmethod
+    def agg_movies_query(movies_query):
+        """Static method with aggregating logic for the Filmwork model"""
+        query_with_vals = movies_query.prefetch_related(
+            'genres', 'personfilmwork',
+        ).values(
+            'id', 'title', 'description', 'creation_date', 'rating', 'type',
         )
+        query_with_aggregated_fields = query_with_vals.annotate(
+            genres=ArrayAgg('genres__name', distinct=True),
+            actors=ArrayAgg(
+                'personfilmwork__person__full_name', distinct=True, filter=Q(personfilmwork__role='actor'),
+            ),
+            writers=ArrayAgg(
+                'personfilmwork__person__full_name', distinct=True, filter=Q(personfilmwork__role='writer'),
+            ),
+            directors=ArrayAgg(
+                'personfilmwork__person__full_name', distinct=True, filter=Q(personfilmwork__role='director'),
+            ),
+        )
+        return query_with_aggregated_fields
+
+    def get_queryset(self):
+        """Method for getting database objects and preparing it"""
+        movies = self.model.objects.all()
+        movies_with_aggregated_fields = self.agg_movies_query(movies)
         return movies_with_aggregated_fields
 
     def get_context_data(self, *, object_list=None, **kwargs):
-        """Method returning dictionary with movies data
-
-        Parameters:
-            object_list
-            **kwargs (dict): Any keyword arguments
-
-        Returns:
-            context (dict): Returns a serialized QuerySet
-        """
+        """Method returning dictionary with movies data"""
         context = {
             'results': list(self.get_queryset()),
         }
         return context
 
     def render_to_response(self, context, **response_kwargs):
-        """Method formatting the response. Returns a response with a
-        structure from the given context.
-
-        Pass response_kwargs to the constructor of the response class.
-        Parameters:
-            context
-            **response_kwargs (dict): Any keyword arguments
-
-        Returns:
-            JsonResponse: Returns a serialized QuerySet
-        """
+        """Method formatting the response. Returns a response with data from the given context."""
         return JsonResponse(context)
 
 
 class SingleMovieApi(MoviesListApi):
     """Represents a view class for serializing information about a movie."""
+
+    def get_queryset(self):
+        """Method for getting database objects and preparing it"""
+        movie = self.model.objects.filter(id=self.kwargs['uuid'])
+        movie_with_aggregated_fields = self.agg_movies_query(movie)
+        return movie_with_aggregated_fields
