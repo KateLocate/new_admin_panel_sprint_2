f11595424396243865a9133784c615381f4bae67
katelocate <katelocat3@gmail.com>
2022-09-01 17:18:29 +0300

    Fix queries for views.

--- 01_docker_compose/app/movies/api/v1/views.py
+++ 01_docker_compose/app/movies/api/v1/views.py
@@ -1,10 +1,7 @@
 """API views for movies app."""
 from django.contrib.postgres.aggregates import ArrayAgg
-
-from django.db.models import Count, Q
-
+from django.db.models import Q
 from django.http import JsonResponse
-from django.views import View
 from django.views.generic.list import BaseListView
 
 from ...models import Filmwork
@@ -32,11 +29,6 @@ class MoviesListApi(BaseListView):
             writers=ArrayAgg('personfilmwork__person__full_name', distinct=True, filter=Q(personfilmwork__role='writer')),
             directors=ArrayAgg('personfilmwork__person__full_name', distinct=True, filter=Q(personfilmwork__role='director')),
         )
-        print(movies_with_aggregated_fields.query)
-        cnt = movies.aggregate(
-            cnt=Count('id')
-        )
-
         return movies_with_aggregated_fields
 
     def get_context_data(self, *, object_list=None, **kwargs):
@@ -49,7 +41,6 @@ class MoviesListApi(BaseListView):
         Returns:
             context (dict): Returns a serialized QuerySet
         """
-
         context = {
             'results': list(self.get_queryset()),
         }
@@ -70,43 +61,5 @@ class MoviesListApi(BaseListView):
         return JsonResponse(context)
 
 
-class SingleMovieApi(View):
+class SingleMovieApi(MoviesListApi):
     """Represents a view class for serializing information about a movie."""
-
-    model = Filmwork
-    http_method_names = ['get']
-
-    def get_queryset(self, uuid):
-        """Method of getting database objects and preparing it"""
-        object = self.model.objects.get(id=uuid)
-        return object
-
-    def get_context_data(self, *, object_list=None, **kwargs):
-        """Method returning dictionary with movies data
-
-        Parameters:
-            object_list
-            **kwargs (dict): Any keyword arguments
-
-        Returns:
-            context (dict): Returns a serialized QuerySet
-        """
-
-        context = {
-            'results': list(self.get_queryset()),
-        }
-        return context
-
-    def render_to_response(self, context, **response_kwargs):
-        """Method formatting the response. Returns a response with a
-        structure from the given context.
-
-        Pass response_kwargs to the constructor of the response class.
-        Parameters:
-            context
-            **response_kwargs (dict): Any keyword arguments
-
-        Returns:
-            JsonResponse: Returns a serialized QuerySet
-        """
-        return JsonResponse(context)
